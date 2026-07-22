3a0f0a1c8d627783fcb0ed635aa14f4bc0021096
Alan <alan.david.marcelino.estrada@gmail.com>
2026-07-22 00:20:54 -0500

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,1 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDfmadkvfnonCEXcUpIFxdmtTYVEkpPtcI",
+  authDomain: "studytrack-58043.firebaseapp.com",
+  projectId: "studytrack-58043",
+  storageBucket: "studytrack-58043.firebasestorage.app",
+  messagingSenderId: "410696716807",
+  appId: "1:410696716807:web:c13c0a0acd6d8b3c1350a2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
